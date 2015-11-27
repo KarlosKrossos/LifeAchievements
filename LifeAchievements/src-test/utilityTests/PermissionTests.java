7f459eeedc4aa222df69d401ca353427806af7d1
StephanBSchuler <sbs.schuler@gmail.com>
2015-11-27 19:48:58 +0100

    test method skeletons
    for standard and special cases

--- LifeAchievements/src-test/utilityTests/PermissionTests.java
+++ LifeAchievements/src-test/utilityTests/PermissionTests.java
@@ -16,16 +16,33 @@ public class PermissionTests {
 
 	@Before
 	public void setUp() throws Exception {
-		guest = new Person("Gustav", "Gr‰uel", 79, true, Permission.GUEST);
+		guest = new Person("Gustav", "Gr√§uel", 79, true, Permission.GUEST);
 		user = new Person("Ulfbert", "Uhrmacher", 800, true, Permission.USER);
 		admin = new Person("Astrid", "Argon", 1, true, Permission.ADMIN);
 	}
-
+	
+	/*
+	* testing whether a specific method can actually really only be accessed/used 
+	* given the respective rights as specified in the PermissionDatabase
+	*/
 	@Test
-	public void test() {
-		// TODO find out how to expect exceptions
-		// TODO then throw them accordingly
-		// fail("Not yet implemented");
+	public void getPermissionTest() {
+		// TODO compare with PermissionDatabase entries(?)
+	}
+	
+	@Test
+	public void getPermissionTest_userNull() {
+		// TODO same test as in getPermissionTest with special condition that person == null
+	}
+	
+	@Test
+	public void getPermissionTest_permissionNull() {
+		// TODO same test as in getPermissionTest with special condition that user == null
+	}
+	
+	@Test
+	public void getPermissionTest_wrongSerialUid() {
+		// TODO compare with PermissionDatabase entries(?)
 	}
 
 }
